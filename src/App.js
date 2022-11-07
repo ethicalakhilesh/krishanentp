be2955e2fe4a7cc4765fb703c799c68b046a1d12
Akhilesh Sharma <akhilesh.sharma421@gmail.com>
2022-11-07 22:46:59 +0530

    uodate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg'
 import './App.css'
 import ColorSchemesExample from './components/navbar'
+import Loginframe from './components/loginFrame';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
-      < ColorSchemesExample />
+      < Loginframe />
     </div>
   );
 }
---
+++ src/components/loginFrame.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Loginframe(){
+    return(
+        <div>
+            <title>Page Title</title>
+            <div style={{ backgroundImage: "url(https://colorlib.com/etc/lf/Login_v16/images/bg-01.jpg)",
+      width: "100%",
+      minHeight: "100vh",
+    }} />
+        </div>
+    );
+}
+
+export default Loginframe;
