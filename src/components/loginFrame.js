9519d80a839105261536ee2aa7fe3a14dc27a513
Akhilesh Sharma <akhilesh.sharma421@gmail.com>
2022-11-07 23:34:18 +0530

    Update loginFrame.js

--- src/components/loginFrame.js
+++ src/components/loginFrame.js
@@ -1,17 +1,45 @@
 import React from "react"
 
-function Loginframe(){
-    return(
+function Loginframe() {
+    return (
         <div>
-            <div style={{ backgroundImage: "url(https://raw.githubusercontent.com/ethicalakhilesh/krishanentp/master/public/images/login-bgnd.jpg)",
+            <div style={{
+                backgroundImage: "url(https://raw.githubusercontent.com/ethicalakhilesh/krishanentp/master/public/images/login-bgnd.jpg)",
                 width: "100%",
                 minHeight: "100vh",
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 position: "relative",
-                padding: 15,
-            }} />
+            }} >
+                <div
+                    style={{
+                        display: "flex",
+                        width: "100%",
+                        height: "100vh",
+                        backgroundColor: "rgba(0,0,0,.65)",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        border: "2px solid black",
+                        padding: 15,
+                    }} >
+                        <div style={{
+                            fontSize: 30,
+                            minWidth: 350,
+                            maxWidth: 500,
+                            height: 350,
+                            backgroundColor: "white",
+                            color: "white",
+                            border: "1px solid white",
+                            borderRadius: 15,
+                        }}>
+                            <img src="https://raw.githubusercontent.com/ethicalakhilesh/krishanentp/master/public/images/krishanentp.jpg" 
+                            style={{
+                                maxWidth: "100%",
+                            }} />
+                        </div>
+                </div>
+            </div>
         </div>
     );
 }
